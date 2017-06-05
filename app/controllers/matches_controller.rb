75a28e300f22c94ecbaf8727c19ed93f38d205b1
kara <kdiaby.pro@gmail.com>
2017-06-05 15:13:23 +0200

    add match controller and index view

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,5 @@
 class MatchesController < ApplicationController
   def index
+    @matches = policy_scope(Match.where("user_1_id = ? OR user_2_id = ?", current_user.id, current_user.id))
   end
 end
