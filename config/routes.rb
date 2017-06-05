31c06a955233a9db8e0e838ce9330593a98e315b
Charles Marcoin <charles.marcoin@gmail.com>
2017-06-05 10:06:09 +0200

    added description to User model and ajax form to change description on user profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update] do
       get 'eventme_pictures_set' => 'users#eventme_pictures_set'
       get 'pictures' => 'users#pictures'
+  # Ici on utilise deux méthodes patchs pour modifier la photo de profil et la description du l'utilisateur avec des requêtes ajax
       patch 'update_picture' => 'users#update_picture'
+      patch 'update_description' => 'users#update_description'
     end
   get "search" => "users#search"
   resources :events, only: [:index]
